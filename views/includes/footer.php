3413ee5cf5a6d4cc6ca5f3c31e5801049938a23c
Mouhamed-Talibi <mouha22talibi@gmail.com>
2025-02-10 20:05:36 +0100

    Editing footer

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -12,10 +12,10 @@
             <div class="col-md-4 mb-4">
                 <h5 class="heading fw-bold">Quick Links</h5>
                 <ul class="links list-unstyled">
-                    <li><a href="" class="text-light text-decoration-none">Home</a></li>
-                    <li><a href="" class="text-light text-decoration-none">Menu</a></li>
-                    <li><a href="" class="text-light text-decoration-none">Services</a></li>
-                    <li><a href="" class="text-light text-decoration-none">Contact Us</a></li>
+                    <li><a href="routes.php?action=customerHome" class="text-light text-decoration-none">Home</a></li>
+                    <li><a href="routes.php?action=menu" class="text-light text-decoration-none">Menu</a></li>
+                    <li><a href="routes.php?action=services" class="text-light text-decoration-none">Services</a></li>
+                    <li><a href="routes.php?action=contact" class="text-light text-decoration-none">Contact Us</a></li>
                 </ul>
             </div>
             <!-- Contact Section -->
@@ -27,8 +27,8 @@
                 <div class="social-icons mt-3">
                     <a href="#" class="text-light me-3"><i class="fab fa-facebook-f"></i></a>
                     <a href="#" class="text-light me-3"><i class="fab fa-twitter"></i></a>
-                    <a href="#" class="text-light me-3"><i class="fab fa-instagram"></i></a>
-                    <a href="#" class="text-light"><i class="fab fa-linkedin-in"></i></a>
+                    <a href="https://www.instagram.com/easy.code_/" class="text-light me-3"><i class="fab fa-instagram"></i></a>
+                    <a href="https://www.linkedin.com/in/mohamed-talibi-639902333/" class="text-light"><i class="fab fa-linkedin-in"></i></a>
                 </div>
             </div>
         </div>
